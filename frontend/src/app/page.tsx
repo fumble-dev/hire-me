968cded48e196e1ee3bf621c869649a49a3d02a3
Udanth <udanth.work@gmail.com>
2025-11-30 15:50:40 +0530

    frontend init

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Hero from "@/components/hero"
+import { Button } from "@/components/ui/button"
+
+const Home = () => {
+  return (
+    <div>
+      <Hero/>
+    </div>
+  )
+}
+
+export default Home
