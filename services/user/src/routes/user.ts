3f130920b4c677818d221491f65a50e836652960
Udanth <udanth.work@gmail.com>
2025-12-25 08:05:52 +0530

    update user added

--- services/user/src/routes/user.ts
+++ services/user/src/routes/user.ts
@@ -1,9 +1,21 @@
 import express from "express";
-import { myProfile } from "../controllers/user.js";
+import {
+  getUserProfile,
+  myProfile,
+  updateProfilePic,
+  updateResume,
+  updateUserProfile,
+} from "../controllers/user.js";
 import { isAuth } from "../middleware/auth.js";
+import uploadFile from "../middleware/multer.js";
 
 const router = express.Router();
 
-router.get("/me",isAuth, myProfile);
+router.get("/me", isAuth, myProfile);
+router.put("/update/profile", isAuth, updateUserProfile);
+router.put("/update/pic", isAuth, uploadFile, updateProfilePic);
+router.put("/update/resume", isAuth, uploadFile, updateResume);
+
+router.get("/:userId", isAuth, getUserProfile);
 
 export default router;
